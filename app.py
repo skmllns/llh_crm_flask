577b099e32973a0fe740c1db61e873a4ae44faa8
skmullins <skmllns@gmail.com>
2015-11-09 12:24:32 -0500

    added pages, applied CSS

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return render_template("index.html")
+  
+@app.route('/add_const.html')
+def add_const():
+  return render_template("add_const.html")
+  
+@app.route('/')
+def main_template():
   return render_template("main_template.html")
 
 app.run(debug=True)
